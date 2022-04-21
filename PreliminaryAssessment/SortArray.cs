daeee9ac56cb9cbe2bbd0e12bceb7d125a497dce
József Krizsán <fysikko@gmail.com>
2022-04-21 17:09:39 +0200

    Add Projects(Skeleton)

---
+++ PreliminaryAssessment/SortArray.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PreliminaryAssessment
+{
+    internal class SortArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
